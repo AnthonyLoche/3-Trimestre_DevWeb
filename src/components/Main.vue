99f23bd4f399301eb2fcf1abefea981bb981c420
AnthonyLoche <anthonylocheifc@gmail.com>
2023-11-29 15:08:55 -0300

    Update package-lock.json and package.json

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -26,6 +26,24 @@
             <h3>Já é um cliente, então faça o login aqui, senão cadastre-se:</h3>
             <router-link to="/login" id="login">Login / Cadastre-Se</router-link>
         </section>
+        <section id="opcoes">
+            <div class="itemOpcoes">
+                <p class="opcoesTenis">Nike</p>
+            <img src="../midia/AirJordanOne.png" class="cem" alt="">
+            </div>
+            <div class="itemOpcoes">
+                <p class="opcoesTenis">Adidas</p>
+            <img src="../midia/young.png" style="margin: auto;" class="cem" alt="">
+            </div>
+            <div class="itemOpcoes">
+                <p class="opcoesTenis">Under Armor</p>
+            <img src="../midia/Under.png" alt="" style="margin-top: 26px;" class="cem">
+            </div>
+            <div class="itemOpcoes">
+                <p class="opcoesTenis">Asics</p>
+                <img src="../midia/asics.png" class="cem" alt="">
+            </div>
+        </section>
     </main>
 </template>
 
@@ -123,4 +141,32 @@ p.jordan{
     text-align: center;
     font-size: 15pt;
 }
+
+#opcoes{
+    width: 100%;
+    display: flex;
+    
+    align-items: center;
+    justify-content: center;
+}
+
+.itemOpcoes{
+    width: 200px;
+    height: 200px;
+    background-color: #282923;
+    margin: 10px;
+    border-radius: 15px;
+}
+
+p.opcoesTenis {
+    text-align: center;
+    font-size: 20pt;
+    color: #282923;
+    background-color: white;
+    width: 100%;
+    margin:  auto;
+    border-radius: 10px 10px 0px 0px;
+    border: 2px solid #282923;
+}
+
 </style>
